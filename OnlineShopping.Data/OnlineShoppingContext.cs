91245d57c184ac7a7bab7b44f1b661e94845d7b7
Amr Software <amrsoftware@outlook.com>
2020-06-11 23:44:15 +0530

    Add project files.

---
+++ OnlineShopping.Data/OnlineShoppingContext.cs
@@ -0,0 +1,13 @@
+﻿namespace OnlineShopping.Data
+{
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore;
+
+    public class OnlineShoppingContext : IdentityDbContext
+    {
+        public OnlineShoppingContext(DbContextOptions<OnlineShoppingContext> options)
+        : base(options)
+        {
+        }
+    }
+}
---
+++ OnlineShopping/Startup.cs
@@ -0,0 +1,52 @@
+namespace OnlineShopping
+{
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Hosting;
+    using OnlineShopping.Data;
+
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddDbContextPool<OnlineShoppingContext>(
+                options => options.UseSqlServer(Configuration.GetConnectionString("OnlineShopping")));
+
+            services.AddIdentity<IdentityUser, IdentityRole>()
+                .AddEntityFrameworkStores<OnlineShoppingContext>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
